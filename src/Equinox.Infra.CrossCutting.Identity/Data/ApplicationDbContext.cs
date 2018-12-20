e5ae9e60bb73577213cc189446bcbf2fa8a83b44
FernandoPucci <fsilvapucci@gmail.com>
2018-12-19 22:14:04 -0200

    Refactor to run in linux

--- src/Equinox.Infra.CrossCutting.Identity/Data/ApplicationDbContext.cs
+++ src/Equinox.Infra.CrossCutting.Identity/Data/ApplicationDbContext.cs
@@ -1,28 +1,41 @@
 ﻿using System.IO;
+using System.Reflection;
 using Equinox.Infra.CrossCutting.Identity.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
-namespace Equinox.Infra.CrossCutting.Identity.Data
-{
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+namespace Equinox.Infra.CrossCutting.Identity.Data {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base (options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
             // get the configuration from the app settings
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
+            var config = new ConfigurationBuilder ()
+                .SetBasePath (Directory.GetCurrentDirectory ())
+                .AddJsonFile ("appsettings.json")
+                .Build ();
 
             // define the database to use
-            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            optionsBuilder.UseSqlServer (config.GetConnectionString ("DefaultConnection"));
         }
     }
+
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext> {
+        public ApplicationDbContext CreateDbContext (string[] args) {
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext> ();
+
+            var config = new ConfigurationBuilder ()
+                .SetBasePath (Directory.GetCurrentDirectory ())
+                .AddJsonFile ("appsettings.json")
+                .Build ();
+
+            builder.UseSqlServer (config.GetConnectionString ("DefaultConnection"),
+                optionsBuilder => optionsBuilder.MigrationsAssembly (typeof (ApplicationDbContext).GetTypeInfo ().Assembly.GetName ().Name));
+
+            return new ApplicationDbContext (builder.Options);
+        }
+    }
+
 }
